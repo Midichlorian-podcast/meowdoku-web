18614f52b1babe5458d0702a88d4b1a5d4645d70
Adam Košir <adam.kosir@edcbrno.com>
2026-09-17 17:24:25 +0200

    Sestavení hry ze zdroje 0107882

--- assets/BufferResource-BHQb99d6.js
+++ assets/BufferResource-BHQb99d6.js
@@ -1,4 +1,4 @@
-import{w as _,k as g,l as c,u as U,m as y,o as l,p as I,q as m}from"./puppet-DgQly2e3.js";const h={name:"local-uniform-bit",vertex:{header:`
+import{w as _,k as g,l as c,u as U,m as y,o as l,p as I,q as m}from"./puppet-dwcapobU.js";const h={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
