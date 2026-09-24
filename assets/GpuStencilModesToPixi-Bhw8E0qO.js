e0285cbe3c3ded06675f0207b09f5b563dd3f197
Adam Košir <adam.kosir@edcbrno.com>
2026-09-24 14:28:53 +0200

    Sestavení hry ze zdroje 5a92e7c

--- assets/GpuStencilModesToPixi-Bhw8E0qO.js
+++ assets/GpuStencilModesToPixi-Bhw8E0qO.js
@@ -1,4 +1,4 @@
-import{w as l,g as c}from"./index-DQVjTmfK.js";import{c as a}from"./App-CFq3g2cQ.js";const m={name:"local-uniform-bit",vertex:{header:`
+import{w as l,g as c}from"./index-CEy1XgE2.js";import{c as a}from"./App-CKPVBLjD.js";const m={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
